c0a75814ec71ac6c5e1037fb6ef909698dce5d7c
Olga Krukova <krukovaolga1@gmail.com>
2024-01-07 19:44:23 +0200

    refactor: substitude tag selectors: h3, label[for=new-task], input[type="text"] to class names; subctitude input#new-task id selector to composed class selector .add__label .add__form .task-text in style.css according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -8,8 +8,9 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addForm=document.forms[0];//Add a new task.
+var taskInput = addForm.elements.task;//Add a new task.
+var addButton=addForm.elements.btn;//Submit button
 var incompleteTaskHolder=document.getElementById("tasks_incompleted");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("tasks_completed");//completed-tasks
 
